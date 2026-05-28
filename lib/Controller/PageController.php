c07219d609dde55bf27053ccf9597ca32abaa53d
simon-pfahler <simon-pfahler@gmx.de>
2026-05-28 13:01:51 +0200

    chore: Update PageController

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -11,19 +11,21 @@ use OCP\AppFramework\Http\Attribute\NoAdminRequired;
 use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
 use OCP\AppFramework\Http\Attribute\OpenAPI;
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IRequest;
 
 /**
  * @psalm-suppress UnusedClass
  */
 class PageController extends Controller {
+    public function __construct($appName, IRequest $request) {
+        parent::__construct($appName, $request);
+    }
 	#[NoCSRFRequired]
 	#[NoAdminRequired]
-	#[OpenAPI(OpenAPI::SCOPE_IGNORE)]
 	#[FrontpageRoute(verb: 'GET', url: '/')]
 	public function index(): TemplateResponse {
 		return new TemplateResponse(
-			Application::APP_ID,
-			'index',
+            'index',
 		);
 	}
 }
